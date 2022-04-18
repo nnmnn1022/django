21ab004e4038bd5a60c6133fa786c74e478f84e3
Umoo <nnmnn1022@naver.com>
2022-04-18 23:15:00 +0900

    0418 advanced mission

--- liongram/support/admin.py
+++ liongram/support/admin.py
@@ -1,6 +1,14 @@
+from tabnanny import verbose
 from django.contrib import admin
 
-from support.models import Faq
+from support.models import Faq, Inquiry, Answer
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    verbose_name = '답변'
+    verbose_name_plural = '답변'
+    extra = 0
+    readonly_fields = ('created_at', 'writer', 'modified_at', 'last_modifier')
 
 # Register your models here.
 @admin.register(Faq)
@@ -11,4 +19,15 @@ class FaqModelAdmin(admin.ModelAdmin):
     search_fields = ('id', 'writer__username')
     search_help_text = '게시판 번호, 작성자 검색이 가능합니다.'
     # 나중에 login 및 session을 배우고 나면 view단에서 생성자, 수정자를 지정해 줄 수 있을 것 같아 일단 생성자, 수정자를 readonly로 지정했음
-    readonly_fields = ('created_at', 'created_by','modified_at', 'last_modifier')
+    readonly_fields = ('created_at', 'modified_at')
+
+@admin.register(Inquiry)
+class InquiryModelAdmin(admin.ModelAdmin): 
+    list_display = ('title','created_by', 'created_at')
+
+    list_filter = ('created_at', 'created_by', 'category')
+    search_fields = ('id', 'writer__username')
+    search_help_text = '게시판 번호, 작성자 검색이 가능합니다.'
+    # 나중에 login 및 session을 배우고 나면 view단에서 생성자, 수정자를 지정해 줄 수 있을 것 같아 일단 생성자를 readonly로 지정했음
+    readonly_fields = ('created_at', 'created_by')
+    inlines = [AnswerInline]
--- liongram/support/models.py
+++ liongram/support/models.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.forms import EmailField
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
@@ -31,3 +32,39 @@ class Faq(models.Model):
     # auto_now : 수정된 날짜 시에 사용
     modified_at = models.DateTimeField(verbose_name='최종 수정일시', auto_now='true')
 
+class Inquiry(models.Model):
+    class Category(models.TextChoices):
+        GENERAL = 'GEN', _('General issue')
+        ACCOUNT = 'ACC', _('Account issue')
+        ETC = 'ETC', _('etc.')
+    category = models.CharField(max_length=3,
+        choices=Category.choices,
+        default=Category.GENERAL)
+
+    title = models.CharField(verbose_name='질문 제목', max_length=64)
+    email = models.EmailField(verbose_name='e-mail')
+    email_checkbox = models.BooleanField(verbose_name='e-mail로 답변을 받겠습니다.')
+
+    phone_number = models.CharField(verbose_name='전화번호', max_length=11)
+    phone_checkbox = models.BooleanField(verbose_name='문자메시지로 답변을 받겠습니다.')
+    detail = models.TextField(verbose_name='내용')
+
+    image = models.ImageField(verbose_name='이미지', upload_to='uploades/', blank=True, null=True)
+    dateTimeOfUpload = models.DateTimeField(auto_now = True)
+    
+    created_at = models.DateTimeField(verbose_name='생성일시', auto_now_add=True)
+    # session에서 유저 데이터를 받아와야 하지만, 아직 배우지 못한 부분이라 blank, null을 True로 처리해서 생성자 데이터가 없어도 데이터 입력 가능하도록 함
+    created_by = models.ForeignKey(to=User, verbose_name='생성자', on_delete=models.CASCADE, related_name='inquiry_creater', blank=True, null=True)
+
+class Answer(models.Model):
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='생성일시', auto_now_add=True)
+    # 외래키와 연동 및 종속되도록 설정
+    Inquiry = models.ForeignKey(to='Inquiry', on_delete=models.CASCADE)
+    # 사용자와 연결하기
+    # session에서 유저 데이터를 받아와야 하지만, 아직 배우지 못한 부분이라 blank, null을 True로 처리해서 생성자 데이터가 없어도 데이터 입력 가능하도록 함
+    writer = models.ForeignKey(to=User, related_name='Answer_writer', on_delete=models.CASCADE, blank=True, null=True)
+
+    last_modifier = models.ForeignKey(to=User, verbose_name='최종 수정자', on_delete=models.CASCADE, related_name='Answer_modifier', blank=True, null=True)
+    # auto_now : 수정된 날짜 시에 사용
+    modified_at = models.DateTimeField(verbose_name='최종 수정일시', auto_now='true')
